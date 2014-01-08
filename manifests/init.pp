77fc33bc97a03a56369e0115b1f1f5864fe80c85
root <root@master.(none)>
2014-01-08 05:35:07 +0000

    Updated

---
+++ manifests/init.pp
@@ -0,0 +1,19 @@
+class node_decommission inherits pe_mcollective::shared_key_files {
+  
+ File {  
+    ensure  => absent,
+    purge   => true,
+    recurse => true,
+    force   => true,
+  }
+  Service {
+    ensure => stopped,
+  }
+    
+  file {'/etc/puppetlabs/puppet/ssl':}
+  
+  file {'/etc/puppetlabs/mcollective/ssl/':}
+ 
+  service {'pe-httpd':}
+
+}
